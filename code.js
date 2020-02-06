ac0e6f7025f8121ed95bc5c7d19a1ed5c7d1d3c4
ahmednooor <m.ahmednoor7@yahoo.com>
2020-02-07 02:40:22 +0500

    v5.0
    - tell user if text is all non-RTL
    
    - improved multi-line formatting

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
-// "RTLit" is a Figma plugin that converts the Arabic/Persian/Urdu text into 
+// "RTLit" is a Figma plugin that converts the Arabic/Persian/Urdu text into
 // readable format as Figma currently does not support RTL languages.
 // author: ahmed noor (github.com/ahmednooor)
 const SPECIAL_CHARS = {
+    // diacritics etc.
     "\u064B": "",
     "\u064C": "",
     "\u064D": "",
@@ -15,100 +16,146 @@ const SPECIAL_CHARS = {
     "\u0655": "",
     "\u0656": "",
     "\u0657": "",
-    "\u0658": "",
+    "\u0658": ""
 };
 const LTR_CHARS = "" +
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" +
     ".,1234567890/*-+_=\\!@#$%^&*;?`~ ()[]{}<>|";
 function isPersoArabicLTRChar(char) {
     // tells you if its numeric etc. since numbers are LTR in arabic
-    return (char.codePointAt(0) > 1775 && char.codePointAt(0) < 1786)
-        || (char.codePointAt(0) > 1631 && char.codePointAt(0) < 1646);
+    return ((char.codePointAt(0) > 1775 && char.codePointAt(0) < 1786) ||
+        (char.codePointAt(0) > 1631 && char.codePointAt(0) < 1646));
 }
 function isLTRChar(char) {
-    return LTR_CHARS.includes(char)
-        || isPersoArabicLTRChar(char);
+    return LTR_CHARS.includes(char) || isPersoArabicLTRChar(char);
+}
+function isAllNonPersoArabic(text) {
+    for (const char of text) {
+        const charCode = char.codePointAt(0);
+        /* start/end codes (converted from hex to dec versions of unicode)
+           courtesy for unicode hex codes:
+           https://en.wikipedia.org/wiki/Arabic_script_in_Unicode */
+        const isArabic = charCode >= 1536 && charCode <= 1791;
+        const isArabicSupplement = charCode >= 1872 && charCode <= 1919;
+        const isArabicExtendedA = charCode >= 2208 && charCode <= 2303;
+        const isArabicPresentFormA = charCode >= 64336 && charCode <= 65023;
+        const isArabicPresentFormB = charCode >= 65136 && charCode <= 65279;
+        const isRumiNumeral = charCode >= 69216 && charCode <= 69247;
+        const isIndicSiyaq = charCode >= 126064 && charCode <= 126143;
+        const isOttomanSiyaq = charCode >= 126208 && charCode <= 126287;
+        const isArabicMathAlphabet = charCode >= 126464 && charCode <= 126719;
+        if (isArabic ||
+            isArabicSupplement ||
+            isArabicExtendedA ||
+            isArabicPresentFormA ||
+            isArabicPresentFormB ||
+            isRumiNumeral ||
+            isIndicSiyaq ||
+            isOttomanSiyaq ||
+            isArabicMathAlphabet) {
+            return false;
+        }
+    }
+    return true;
+}
+function offsetSpecialCharsForReversal(charStreamArr) {
+    /* make sure special chars such as diacritics shouldn't be reversed
+       since they come after a char not before */
+    for (let i = 1; i < charStreamArr.length; i++) {
+        if (SPECIAL_CHARS[charStreamArr[i]] !== undefined) {
+            const tempChar = charStreamArr[i - 1];
+            charStreamArr[i - 1] = charStreamArr[i];
+            charStreamArr[i] = tempChar;
+        }
+    }
+    return charStreamArr;
+}
+function breakLTRAndRTLParts(charStreamArr) {
+    let brokenLTRAndRTLParts = [""];
+    let LTRCharIndices = [];
+    /* make sure LTR chars such as English or arabic numerals remain LTR */
+    for (let i = 0; i < charStreamArr.length; i++) {
+        if (isLTRChar(charStreamArr[i])) {
+            LTRCharIndices.push(i);
+            continue;
+        }
+        if (LTRCharIndices.length > 0) {
+            brokenLTRAndRTLParts.push(charStreamArr
+                .slice(LTRCharIndices[0], i).reverse().join(""));
+            LTRCharIndices = [];
+        }
+        brokenLTRAndRTLParts[brokenLTRAndRTLParts.length - 1] +=
+            charStreamArr[i];
+    }
+    if (LTRCharIndices.length > 0) {
+        brokenLTRAndRTLParts.push(charStreamArr
+            .slice(LTRCharIndices[0], LTRCharIndices[LTRCharIndices.length])
+            .reverse().join(""));
+        LTRCharIndices = [];
+    }
+    return brokenLTRAndRTLParts;
 }
 for (let node of figma.currentPage.selection) {
-    if (node.type !== 'TEXT') {
+    if (node.type !== "TEXT") {
         figma.closePlugin("Please select a text box that contains Perso-Arabic text " +
             "and re-run the plugin.");
     }
-    if (node.type === 'TEXT') {
-        figma.loadFontAsync({ family: node.fontName["family"],
-            style: node.fontName["style"] })
-            .then((_ => {
-            let finalStream = [];
+    if (node.type === "TEXT" && isAllNonPersoArabic(node["characters"])) {
+        figma.closePlugin("The selected text box does not contain Perso-Arabic text. " +
+            "Please try again with Perso-Arabic text.");
+    }
+    if (node.type === "TEXT") {
+        figma.loadFontAsync({
+            family: node.fontName["family"],
+            style: node.fontName["style"]
+        }).then(_ => {
             // console.log(node["characters"].length);
+            let finalStream = [];
             for (let charStream of node["characters"].split("\n")) {
-                let charStreamArr = charStream.split("");
-                /* make sure special chars such as diacritics shouldn't be reversed
-                   since they come after a char not before */
-                for (let i = 1; i < charStreamArr.length; i++) {
-                    if (SPECIAL_CHARS[charStreamArr[i]] !== undefined) {
-                        const tempChar = charStreamArr[i - 1];
-                        charStreamArr[i - 1] = charStreamArr[i];
-                        charStreamArr[i] = tempChar;
-                    }
-                }
-                /* ------------------------------------------------------ */
-                charStream = "";
-                let LTRCharIndices = [];
-                /* make sure LTR chars such as English or arabic numerals remain LTR */
-                for (let i = 0; i < charStreamArr.length; i++) {
-                    if (isLTRChar(charStreamArr[i])) {
-                        LTRCharIndices.push(i);
-                        continue;
-                    }
-                    if (LTRCharIndices.length > 0) {
-                        charStream += charStreamArr.slice(LTRCharIndices[0], i)
-                            .reverse().join("");
-                        LTRCharIndices = [];
-                    }
-                    charStream += charStreamArr[i];
-                }
-                if (LTRCharIndices.length > 0) {
-                    charStream += charStreamArr
-                        .slice(LTRCharIndices[0], LTRCharIndices[LTRCharIndices.length])
-                        .reverse().join("");
-                    LTRCharIndices = [];
-                }
-                /* ------------------------------------------------------ */
-                charStream = charStream.split("").reverse().join("");
-                let wordsArr = charStream.split(" ");
-                charStream = wordsArr.join(" ");
+                let charStreamArr = offsetSpecialCharsForReversal(charStream.split(""));
+                let brokenLTRAndRTLParts = breakLTRAndRTLParts(charStreamArr);
+                charStream = brokenLTRAndRTLParts.join("");
+                /* simple case of single-line. reverse all chars */
                 if (node["textAutoResize"] === "WIDTH_AND_HEIGHT") {
-                    finalStream.push(charStream);
+                    finalStream.push(charStream.split("").reverse().join(""));
                     continue;
                 }
-                wordsArr = wordsArr.reverse();
+                /* complex case of multi-line.
+                   reverse in a way that is readable from top to bottom */
                 let tempNode = node.clone();
-                tempNode["characters"] = wordsArr[0];
+                tempNode["characters"] = charStream.split(" ")[0];
                 tempNode["textAutoResize"] = "HEIGHT";
                 let tempNodeInitialHeight = tempNode["height"];
                 let linesArr = [];
-                let lineWords = [];
                 tempNode["characters"] = "";
-                /* reverse words/lines in a way that is readable from top to bottom */
-                for (const word of wordsArr) {
-                    tempNode["characters"] += word;
-                    if (tempNode["height"] !== tempNodeInitialHeight) {
-                        linesArr.push(lineWords.reverse().join(" "));
-                        tempNode["characters"] = word;
-                        lineWords = [];
+                for (const char of charStream) {
+                    tempNode["characters"] = char + tempNode["characters"];
+                    if (tempNode["height"] > tempNodeInitialHeight) {
+                        let charStreamToPush = tempNode["characters"];
+                        let charStreamToPushWords = charStreamToPush.split(" ");
+                        if (charStreamToPushWords.length > 1) {
+                            tempNode["characters"] =
+                                charStreamToPushWords[0];
+                            charStreamToPush = charStreamToPushWords
+                                .slice(1, charStreamToPushWords.length)
+                                .join(" ");
+                        }
+                        else {
+                            tempNode["characters"] =
+                                charStreamToPush.substring(0, 1);
+                            charStreamToPush = charStreamToPush.substring(1, charStreamToPush.length);
+                        }
+                        linesArr.push(charStreamToPush);
                     }
-                    tempNode["characters"] += " ";
-                    lineWords.push(word);
                 }
-                linesArr.push(lineWords.reverse().join(" "));
-                /* ------------------------------------------------------ */
-                charStream = linesArr.join(" ");
+                linesArr.push(tempNode["characters"]);
+                charStream = linesArr.join("\n");
                 finalStream.push(charStream);
                 tempNode.remove();
             }
             node["characters"] = finalStream.join("\n");
             // console.log(node["characters"].length);
-        }));
+        });
     }
 }
 // Make sure to close the plugin when you're done. Otherwise the plugin will
--- code.ts
+++ code.ts
@@ -1,135 +1,192 @@
-// "RTLit" is a Figma plugin that converts the Arabic/Persian/Urdu text into 
+// "RTLit" is a Figma plugin that converts the Arabic/Persian/Urdu text into
 // readable format as Figma currently does not support RTL languages.
 // author: ahmed noor (github.com/ahmednooor)
 
-const SPECIAL_CHARS = { // diacritics etc.
-  "\u064B": "",
-  "\u064C": "",
-  "\u064D": "",
-  "\u064E": "",
-  "\u064F": "",
-  "\u0650": "",
-  "\u0651": "",
-  "\u0652": "",
-  "\u0653": "",
-  "\u0654": "",
-  "\u0655": "",
-  "\u0656": "",
-  "\u0657": "",
-  "\u0658": "",
+const SPECIAL_CHARS = {
+    // diacritics etc.
+    "\u064B": "",
+    "\u064C": "",
+    "\u064D": "",
+    "\u064E": "",
+    "\u064F": "",
+    "\u0650": "",
+    "\u0651": "",
+    "\u0652": "",
+    "\u0653": "",
+    "\u0654": "",
+    "\u0655": "",
+    "\u0656": "",
+    "\u0657": "",
+    "\u0658": ""
 };
 
-const LTR_CHARS = "" +
-"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-".,1234567890/*-+_=\\!@#$%^&*;?`~ ()[]{}<>|"
+const LTR_CHARS =
+    "" +
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+    ".,1234567890/*-+_=\\!@#$%^&*;?`~ ()[]{}<>|";
 
-function isPersoArabicLTRChar(char) {
-  // tells you if its numeric etc. since numbers are LTR in arabic
-  return (char.codePointAt(0) > 1775 && char.codePointAt(0) < 1786)
-         || (char.codePointAt(0) > 1631 && char.codePointAt(0) < 1646);
+function isPersoArabicLTRChar(char: string): boolean {
+    // tells you if its numeric etc. since numbers are LTR in arabic
+    return (
+        (char.codePointAt(0) > 1775 && char.codePointAt(0) < 1786) ||
+        (char.codePointAt(0) > 1631 && char.codePointAt(0) < 1646)
+    );
 }
 
-function isLTRChar(char) {
-  return LTR_CHARS.includes(char) 
-         || isPersoArabicLTRChar(char);
+function isLTRChar(char: string): boolean {
+    return LTR_CHARS.includes(char) || isPersoArabicLTRChar(char);
+}
+
+function isAllNonPersoArabic(text: string): boolean {
+    for (const char of text) {
+        const charCode = char.codePointAt(0);
+
+        /* start/end codes (converted from hex to dec versions of unicode)
+           courtesy for unicode hex codes:
+           https://en.wikipedia.org/wiki/Arabic_script_in_Unicode */
+        const isArabic = charCode >= 1536 && charCode <= 1791;
+        const isArabicSupplement = charCode >= 1872 && charCode <= 1919;
+        const isArabicExtendedA = charCode >= 2208 && charCode <= 2303;
+        const isArabicPresentFormA = charCode >= 64336 && charCode <= 65023;
+        const isArabicPresentFormB = charCode >= 65136 && charCode <= 65279;
+        const isRumiNumeral = charCode >= 69216 && charCode <= 69247;
+        const isIndicSiyaq = charCode >= 126064 && charCode <= 126143;
+        const isOttomanSiyaq = charCode >= 126208 && charCode <= 126287;
+        const isArabicMathAlphabet = charCode >= 126464 && charCode <= 126719;
+
+        if (
+            isArabic ||
+            isArabicSupplement ||
+            isArabicExtendedA ||
+            isArabicPresentFormA ||
+            isArabicPresentFormB ||
+            isRumiNumeral ||
+            isIndicSiyaq ||
+            isOttomanSiyaq ||
+            isArabicMathAlphabet
+        ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function offsetSpecialCharsForReversal(charStreamArr: string[]): string[] {
+    /* make sure special chars such as diacritics shouldn't be reversed
+       since they come after a char not before */
+    for (let i = 1; i < charStreamArr.length; i++) {
+        if (SPECIAL_CHARS[charStreamArr[i]] !== undefined) {
+            const tempChar = charStreamArr[i - 1];
+            charStreamArr[i - 1] = charStreamArr[i];
+            charStreamArr[i] = tempChar;
+        }
+    }
+    return charStreamArr;
+}
+
+function breakLTRAndRTLParts(charStreamArr: string[]): string[] {
+    let brokenLTRAndRTLParts = [""];
+    let LTRCharIndices = [];
+
+    /* make sure LTR chars such as English or arabic numerals remain LTR */
+    for (let i = 0; i < charStreamArr.length; i++) {
+        if (isLTRChar(charStreamArr[i])) {
+            LTRCharIndices.push(i);
+            continue;
+        }
+        if (LTRCharIndices.length > 0) {
+            brokenLTRAndRTLParts.push(charStreamArr
+                .slice(LTRCharIndices[0], i).reverse().join(""));
+            LTRCharIndices = [];
+        }
+        brokenLTRAndRTLParts[brokenLTRAndRTLParts.length - 1] += 
+            charStreamArr[i];
+    }
+    if (LTRCharIndices.length > 0) {
+        brokenLTRAndRTLParts.push(charStreamArr
+            .slice(LTRCharIndices[0], LTRCharIndices[LTRCharIndices.length])
+            .reverse().join(""));
+        LTRCharIndices = [];
+    }
+
+    return brokenLTRAndRTLParts;
 }
 
 for (let node of figma.currentPage.selection) {
-  if (node.type !== 'TEXT') {
-    figma.closePlugin(
-      "Please select a text box that contains Perso-Arabic text " +
-      "and re-run the plugin."
-    );
-  }
-  if (node.type === 'TEXT') {
-    figma.loadFontAsync({ family: node.fontName["family"], 
-                          style: node.fontName["style"] })
-    .then((_ => {
-      let finalStream = [];
-      // console.log(node["characters"].length);
-      
-      for (let charStream of node["characters"].split("\n")) {
-        let charStreamArr: string[] = charStream.split("");
-        
-        /* make sure special chars such as diacritics shouldn't be reversed
-           since they come after a char not before */
-        for (let i = 1; i < charStreamArr.length; i++) {
-          if (SPECIAL_CHARS[charStreamArr[i]] !== undefined) {
-            const tempChar = charStreamArr[i-1];
-            charStreamArr[i-1] = charStreamArr[i];
-            charStreamArr[i] = tempChar;
-          }
-        }
-        /* ------------------------------------------------------ */
-        
-        charStream = "";
-        let LTRCharIndices = [];
-        
-        /* make sure LTR chars such as English or arabic numerals remain LTR */
-        for (let i = 0; i < charStreamArr.length; i++) {
-          if (isLTRChar(charStreamArr[i])) {
-            LTRCharIndices.push(i);
-            continue;
-          }
-          if (LTRCharIndices.length > 0) {
-            charStream += charStreamArr.slice(LTRCharIndices[0], i)
-                                       .reverse().join("");
-            LTRCharIndices = [];
-          }
-          charStream += charStreamArr[i];
-        }
-        if (LTRCharIndices.length > 0) {
-          charStream += charStreamArr
-            .slice(LTRCharIndices[0], LTRCharIndices[LTRCharIndices.length])
-            .reverse().join("");
-          LTRCharIndices = [];
-        }
-        /* ------------------------------------------------------ */
-        
-        charStream = charStream.split("").reverse().join("");
-        let wordsArr: string[] = charStream.split(" ");
-        charStream = wordsArr.join(" ");
+    if (node.type !== "TEXT") {
+        figma.closePlugin(
+            "Please select a text box that contains Perso-Arabic text " +
+            "and re-run the plugin."
+        );
+    }
 
-        if (node["textAutoResize"] === "WIDTH_AND_HEIGHT") {
-          finalStream.push(charStream);
-          continue;
-        }
+    if (node.type === "TEXT" && isAllNonPersoArabic(node["characters"])) {
+        figma.closePlugin(
+            "The selected text box does not contain Perso-Arabic text. " +
+            "Please try again with Perso-Arabic text."
+        );
+    }
 
-        wordsArr = wordsArr.reverse();
-        let tempNode = node.clone();
-        tempNode["characters"] = wordsArr[0];
-        tempNode["textAutoResize"] = "HEIGHT";
-        let tempNodeInitialHeight = tempNode["height"];
-        let linesArr: string[] = [];
-        let lineWords = []
-        tempNode["characters"] = "";
-        
-        /* reverse words/lines in a way that is readable from top to bottom */
-        for (const word of wordsArr) {
-          tempNode["characters"] += word;
-          
-          if (tempNode["height"] !== tempNodeInitialHeight) {
-            linesArr.push(lineWords.reverse().join(" "));
-            tempNode["characters"] = word;
-            lineWords = []
-          }
-          
-          tempNode["characters"] += " ";
-          lineWords.push(word)
-        }
-        linesArr.push(lineWords.reverse().join(" "));
-        /* ------------------------------------------------------ */
+    if (node.type === "TEXT") {
+        figma.loadFontAsync({
+            family: node.fontName["family"],
+            style: node.fontName["style"]
+        }).then(_ => {
+            // console.log(node["characters"].length);
+            let finalStream = [];
+            for (let charStream of node["characters"].split("\n")) {
+                let charStreamArr = 
+                    offsetSpecialCharsForReversal(charStream.split(""));
+                let brokenLTRAndRTLParts = breakLTRAndRTLParts(charStreamArr);
+                charStream = brokenLTRAndRTLParts.join("");
 
-        charStream = linesArr.join(" ");
-        finalStream.push(charStream);
-        tempNode.remove();
-      }
+                /* simple case of single-line. reverse all chars */
+                if (node["textAutoResize"] === "WIDTH_AND_HEIGHT") {
+                    finalStream.push(charStream.split("").reverse().join(""));
+                    continue;
+                }
 
-      node["characters"] = finalStream.join("\n");
+                /* complex case of multi-line. 
+                   reverse in a way that is readable from top to bottom */
+                let tempNode = node.clone();
+                tempNode["characters"] = charStream.split(" ")[0];
+                tempNode["textAutoResize"] = "HEIGHT";
+                let tempNodeInitialHeight = tempNode["height"];
+                let linesArr = [];
+                tempNode["characters"] = "";
 
-      // console.log(node["characters"].length);
-    }));
-  }
+                for (const char of charStream) {
+                    tempNode["characters"] = char + tempNode["characters"];
+
+                    if (tempNode["height"] > tempNodeInitialHeight) {
+                        let charStreamToPush = tempNode["characters"];
+                        let charStreamToPushWords = charStreamToPush.split(" ");
+
+                        if (charStreamToPushWords.length > 1) {
+                            tempNode["characters"] =
+                                charStreamToPushWords[0];
+                            charStreamToPush = charStreamToPushWords
+                                .slice(1, charStreamToPushWords.length)
+                                .join(" ");
+                        } else {
+                            tempNode["characters"] = 
+                                charStreamToPush.substring(0, 1);
+                            charStreamToPush = charStreamToPush.substring(1,
+                                charStreamToPush.length);
+                        }
+                        linesArr.push(charStreamToPush);
+                    }
+                }
+                linesArr.push(tempNode["characters"]);
+                charStream = linesArr.join("\n");
+
+                finalStream.push(charStream);
+                tempNode.remove();
+            }
+            node["characters"] = finalStream.join("\n");
+            // console.log(node["characters"].length);
+        });
+    }
 }
 
 // Make sure to close the plugin when you're done. Otherwise the plugin will
